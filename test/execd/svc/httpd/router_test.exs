6ee1ea7bf9e5a44ea2dc887bedbf919200cf6ddb
andyl <andy@r210.com>
2024-02-17 10:59:20 -0800

    Add routes and router tests

---
+++ test/execd/svc/httpd/router_test.exs
@@ -0,0 +1,38 @@
+defmodule Execd.Svc.Httpd.RouterTest do
+  use ExUnit.Case
+  import Plug.Test
+
+  alias Execd.Svc.Httpd.Router
+
+  @opts Router.init([])
+
+  describe "/" do
+    test "GET /" do
+      conn =
+        conn(:get, "/")
+        |> Router.call(@opts)
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert conn.resp_body == ":ok"
+    end
+  end
+
+  describe "/submit" do
+    test "POST /submit with form data" do
+      simulated_form_data = "name=John&age=30"
+
+      conn =
+        conn(:post, "/submit", simulated_form_data)
+        |> Plug.Conn.put_req_header("content-type", "application/x-www-form-urlencoded")
+        |> Router.call(@opts)
+
+      assert conn.state == :sent
+      assert conn.status == 200
+      assert conn.resp_body == ":form-data-ok"
+      assert conn.params["age"] == "30"
+      assert conn.params["name"] == "John"
+    end
+  end
+end
+
