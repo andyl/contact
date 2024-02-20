bc77bd3a766140e99b09dd16c07ba1c0d7cf3243
andyl <andy@r210.com>
2024-02-20 07:46:25 -0800

    Drop localhost from message

--- lib/execd/svc/httpd/server.ex
+++ lib/execd/svc/httpd/server.ex
@@ -6,7 +6,7 @@ defmodule Execd.Svc.Httpd.Server do
   alias Execd.Util
 
   def child_spec(_) do
-    Util.IO.puts("Starting Httpd on https://localhost:5001")
+    Util.IO.puts("Starting Httpd on port 5001")
     Supervisor.child_spec(
       {Bandit, scheme: :http, plug: Router, port: 5001},
       []
