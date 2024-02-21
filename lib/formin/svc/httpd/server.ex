fa153f9f2750f39bd4edfe96cd55c27546582488
andyl <andy@r210.com>
2024-02-20 21:08:47 -0800

    Add configurable ports

--- lib/formin/svc/httpd/server.ex
+++ lib/formin/svc/httpd/server.ex
@@ -6,9 +6,11 @@ defmodule Formin.Svc.Httpd.Server do
   alias Formin.Util
 
   def child_spec(_) do
-    Util.IO.puts("Starting Httpd on port 5001")
+    port = Application.get_env(:formin, :port)
+
+    Util.IO.puts("Starting Httpd on port #{port}")
     Supervisor.child_spec(
-      {Bandit, scheme: :http, plug: Router, port: 5001},
+      {Bandit, scheme: :http, plug: Router, port: port},
       []
     )
   end
