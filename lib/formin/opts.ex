e99ea13f6dc86cf46b5a73cc0f45a7020996c615
andyl <andy@r210.com>
2024-02-26 20:21:30 -0800

    Add opts module

---
+++ lib/formin/opts.ex
@@ -0,0 +1,12 @@
+defmodule Formin.Opts do
+
+  def routes do
+    %{
+      "post/contact" => [
+        logfile: "/home/aleak/zz/formin/contact.log"
+      ],
+      "post/wom" => []
+    }
+  end
+
+end
