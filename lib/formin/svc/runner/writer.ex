fdd1bdf2d05f14655dac80a2393dfc1dca976cb6
andyl <andy@r210.com>
2024-02-27 19:30:07 -0800

    Add functions for stdout, logger, file

--- lib/formin/svc/runner/writer.ex
+++ lib/formin/svc/runner/writer.ex
@@ -3,14 +3,42 @@ defmodule Formin.Svc.Runner.Writer do
 
   use GenServer
 
-  alias Formin.Util
+  # alias Formin.Util
+
+  require Logger
 
   @procname :runner_writer
 
+  # ----- startup
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, "", name: @procname)
+  end
+
+  @doc false
+  def init(_) do
+    {:ok, %{}}
+  end
+
   # ----- api
 
-  def output(:logfile, path, json) do
-    GenServer.call(@procname, {:wr_logfile, path, json})
+  def flush do
+    GenServer.call(@procname, :rw_flush)
+    :ok
+  end
+
+  def output(:file, path, json) do
+    GenServer.cast(@procname, {:rw_file, path, json})
+    :ok
+  end
+
+  def output(:logger, pattern, json) do
+    GenServer.cast(@procname, {:rw_logger, pattern, json})
+    :ok
+  end
+
+  def output(:stdout, pattern, json) do
+    GenServer.cast(@procname, {:rw_stdout, pattern, json})
     :ok
   end
 
@@ -34,24 +62,32 @@ defmodule Formin.Svc.Runner.Writer do
     :ok
   end
 
-  # ----- startup
-
-  def start_link(_) do
-    Util.IO.puts("Starting Writer")
-    GenServer.start_link(__MODULE__, "", name: @procname)
-  end
-
-  @doc false
-  def init(_) do
-    {:ok, %{}}
-  end
-
   # ----- callbacks
 
-  def handle_cast({:wr_logfile, path, json}, state) do
+  def handle_call(:rw_flush, _caller, state) do
+    {:reply, :ok, state}
+  end
+
+  def handle_cast({:rw_stdout, pattern, json}, state) do
+    regex = Regex.compile!(pattern)
+    if String.match?(json, regex) do
+      IO.puts(json)
+    end
+    {:noreply, state}
+  end
+
+  def handle_cast({:rw_file, path, json}, state) do
     {:ok, file} = File.open(path, [:append])
     IO.write(file, json)
     File.close(file)
+    {:noreply, state}
+  end
+
+  def handle_cast({:rw_logger, pattern, json}, state) do
+    regex = ~r/#{pattern}/
+    if String.match?(json, regex) do
+      Logger.info(json)
+    end
     {:noreply, state}
   end
 
