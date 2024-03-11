bb8f9321e79118613af8c12b07924d7bc1460cab
andyl <andy@r210.com>
2024-03-11 13:55:33 -0700

    Add Agent state handling

--- lib/formin/cfg/opts.ex
+++ lib/formin/cfg/opts.ex
@@ -1,6 +1,12 @@
-defmodule Formin.Opts do
+defmodule Formin.Cfg.Opts do
 
-  def routes do
+  @procname :formin_opts
+
+  use Agent
+
+  # ----- util
+
+  def default_routes do
     %{
       "post/contact" => [
         logfile: "/home/aleak/zz/formin/contact.log"
@@ -8,5 +14,31 @@ defmodule Formin.Opts do
       "post/wom" => []
     }
   end
+
+  # ----- api
+
+  def set_state(newstate) do
+    Agent.update(@procname, fn _ -> newstate end)
+  end
+
+  def clear_state do
+    Agent.update(@procname, fn _ -> %{} end)
+  end
+
+  def merge_state(newstate) do
+    Agent.update(@procname, fn oldstate -> Map.merge(oldstate, newstate) end)
+  end
+
+  def get_state do
+    Agent.get(@procname, &(&1))
+  end
+
+  # ----- startup
+
+  def start_link(initial_state \\ %{}) when is_map(initial_state) do
+    Agent.start_link(fn -> initial_state end, name: @procname)
+  end
+
+  # ----- helpers
 
 end
